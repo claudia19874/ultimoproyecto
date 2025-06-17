ff62275ad202b511d736703ecac700218d4a6b08
claudia19874 <chrojas.24@est.ucab.edu.ve>
2025-06-17 19:21:14 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
 #prueba
+#include <iostream> 
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+//estructura para archivos
+struct File {
+    char* name; // Nombre del archivo
+    char* content; // Contenido del archivo
+    File* next; // Puntero al siguiente archivo
+};
