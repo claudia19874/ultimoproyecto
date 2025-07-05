b4d193a885b7c673006883376b7d5f0ab71adf3f
claudia19874 <chrojas.24@est.ucab.edu.ve>
2025-07-05 17:11:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,3 +10,11 @@ struct File {
     char* content; // Contenido del archivo
     File* next; // Puntero al siguiente archivo
 };
+
+struct Directory {
+    char* name;           
+    Directory* parent;    // Directorio padre
+    Directory* childDir;  // Lista de subdirectorios 
+    Directory* nextDir;   
+    File* files;          // Lista de archivos 
+};
